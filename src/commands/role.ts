ae392a9cbad20ffada1a0006d251d6b27f3fa07b
Isaiah Cross <icross028@gmail.com>
2020-09-06 02:58:44 -0500

    Created the command for assigning roles to users

---
+++ src/commands/role.ts
@@ -0,0 +1,23 @@
+import { Message, GuildMember, Role } from 'discord.js';
+
+export default function role(msg: Message, username: string, roleName: string): void {
+  // Finding the user
+  const user: GuildMember | null | undefined = msg.guild?.member(msg.mentions.users.first() || username);
+  if (!user) {
+    msg.channel.send('User not found');
+    return;
+  }
+
+  // Finding the role
+  const roleToAssign: Role | undefined = msg.guild?.roles.cache.find((role_) => role_.name === roleName);
+  if (!roleToAssign) {
+    msg.channel.send('Role does not exist');
+    return;
+  }
+
+  // Assigning the user to the role
+  user.roles.add(roleToAssign.id);
+
+  // Logging
+  console.log(`${msg.author.username} has assigned ${user.displayName} to the ${roleToAssign.name} role.`);
+}
